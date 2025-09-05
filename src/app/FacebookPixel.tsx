46fd123d0373393746e6f238728a7651cdb85db2
Jakub Hampl <entitak@gmail.com>
2025-09-05 11:31:28 +0200

    fixing client side for imports

--- src/app/FacebookPixel.tsx
+++ src/app/FacebookPixel.tsx
@@ -1,11 +1,17 @@
 "use client"
 import React, { useEffect } from 'react'
-import ReactPixel from 'react-facebook-pixel'
 
 export default function FacebookPixelProvider({ children }: { children: React.ReactNode }) {
+  const initPixel = async () => {
+    if (typeof window !== 'undefined') {
+      const ReactPixel = (await import('react-facebook-pixel')).default;
+      const pixelId = process.env.NEXT_PUBLIC_PIXEL_ID || ''
+      ReactPixel.init(pixelId)
+    }
+  }
+
   useEffect(() => {
-    const pixelId = process.env.NEXT_PUBLIC_PIXEL_ID || ''
-    ReactPixel.init(pixelId)
+    initPixel()
   }, [])
 
   return children
--- src/components/PruductsPage/App.tsx
+++ src/components/PruductsPage/App.tsx
@@ -4,11 +4,17 @@ import { AppWrapperStyled } from '../Homepage/App.style'
 import ProductsTitle from './ProductsTitle'
 import Products from './Products'
 import { useEffect } from "react";
-import ReactPixel from 'react-facebook-pixel'
 
 export default function App() {
+  const emitPageView = async () => {
+    if (typeof window !== 'undefined') {
+      const ReactPixel = (await import('react-facebook-pixel')).default;
+      ReactPixel.pageView()
+    }
+  }
+
   useEffect(() => {
-    ReactPixel.pageView()
+    emitPageView()
   }, [])
 
   return (
